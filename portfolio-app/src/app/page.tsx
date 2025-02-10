cb6bf9feac1ce94aeb2d668a3847d326f9523c29
Mehrad Hassanpour <mehradhassanp@gmail.com>
2025-02-10 16:37:24 -0500

    Added the other cards

--- portfolio-app/src/app/page.tsx
+++ portfolio-app/src/app/page.tsx
@@ -16,13 +16,17 @@ export default function Home() {
       <div className="mb-8 text-center">
         <h1 className="text-3xl font-bold tracking-tight sm:text-4xl">Experience & Education</h1>
         <p className="mt-4 text-muted-foreground md:text-xl">
-          Check out some of my latest projects.
+          Check out my qualifications and past experiences.
         </p>
       </div>
         <div className="sm:grid flex flex-col items-center md:grid-cols-2 lg:grid-cols-3 gap-8">
           <Prestige />
           <Degree />
           <TA142 />
+          <SQL />
+          <TA214 />
+          <LinkedInCertificate />
+          <
         </div>
       </div>
       <Project></Project>
